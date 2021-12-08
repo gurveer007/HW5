6aa09dd219ee409af630f0310ab5f45a63f9e164
KevLiu5 <kevzliu5@gmail.com>
2021-12-08 01:03:17 -0500

    deleted echo

--- echoservert.c
+++ echoservert.c
@@ -39,3 +39,16 @@ void *thread(void *vargp)
     return NULL;
 }
 /* $end echoservertmain */
+
+void echo(int connfd) 
+{
+    size_t n; 
+    char buf[MAXLINE]; 
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
+	printf("server received %d bytes\n", (int)n);
+	Rio_writen(connfd, buf, n);
+    }
+}
