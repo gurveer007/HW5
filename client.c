b5d1b0af50e3e45bf314d34e50c5f7e2c20cada2
Gurveer S. Aulakh <gsa45@ilab3.cs.rutgers.edu>
2021-12-11 19:49:10 -0500

    sfsd

--- client.c
+++ client.c
@@ -258,8 +258,21 @@ int main(int argc, char* argv[])
 
     //Receiving data from server
     Rio_readlineb(&rio, buf, MAXLINE);
-    //do parsing here and save local changes
     
+    //do parsing here and save local changes
+    for (int y = 0; y < GRIDSIZE; y++) {
+        for (int x = 0; x < GRIDSIZE; x++) {
+            if (player1.x == x && player1.y == y) {
+                strcat(buf, "5,");
+            }
+            else if (grid[x][y] == TILE_TOMATO) {
+                strcat(buf, "1,");
+            }
+            else {
+                strcat(buf, "0,");
+            }
+        }
+    }
 
     //initGrid();
 
--- test.c
+++ test.c
@@ -1,21 +1,62 @@
-/* strcat example */
-#include <stdio.h>
-#include <string.h>
-
-int main ()
+#include<string.h>
+#include<stdio.h>
+int main()
 {
-  char str[80];
-  strcpy (str,"this ");
-  strcat (str,"number ");
-  strcat (str,"is ");
-  int num = 100;
-//   strcpy (str,num);
-//   sprintf();
-//   strcat (str,num);
+    char buf[200];
+    //encoding the grid into buf (100 chars)
+    for (int y = 0; y < 100; y++) {
+        for (int x = 0; x < 100; x++) {
+                if (x==0 && y==0)
+                {
+                    strcpy(buf,"0,");
+                }else{
+                    strcat(buf, "0,");
+                }
+                
+                
+            
+        }
+    }
     char temp[10];
-    sprintf(temp, "%d", num);
-    // printf("%s",temp);
-    strcat (str,temp);
-  puts (str);
-  return 0;
+    int score = 10;
+    int numTomatoes = 9;
+    int level = 7;
+    int localPlayerId = 1;
+
+    sprintf(temp, "%d", score);
+    strcat(buf, temp);
+    strcat(buf, ",");
+
+    sprintf(temp, "%d", numTomatoes);
+    strcat(buf, temp);
+    strcat(buf, ",");
+
+    sprintf(temp, "%d", level);
+    strcat(buf, temp);
+    strcat(buf, ",");
+    
+    sprintf(temp, "%d", localPlayerId);
+    strcat(buf, temp);
+    strcat(buf, ",");
+
+    //replacing last "," with termination character
+    // if (buf != -1) {
+        // buf[strlen(buf)-1] = '\0';
+    // }
+    
+    // char buf[100] = "hello is";
+    char *p;
+    p = strtok(buf, " ");
+
+    
+    for (size_t i = 0; i < strlen(buf); i++)
+    {
+        if(p)
+        {
+            printf("%s\n", p);
+        }
+        p = strtok(NULL, ",");
+    }
+    
+    return 0;
 }
